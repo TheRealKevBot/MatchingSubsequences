abc93dca55c33ae4b2a7be4f82d309ef9814c9a7
TheRealKevBot <Kglas001@gmail.com>
2021-06-22 15:25:24 -0600

    First

---
+++ index.js
@@ -0,0 +1,14 @@
+// Given a string s and an array of strings words, 
+// return the number of words[i] that is a subsequence of s.
+
+// A subsequence of a string is a new string generated from 
+// the original string with some characters (can be none) deleted without changing 
+// the relative order of the remaining characters.
+// For example, "ace" is a subsequence of "abcde". 
+
+function numMatchingSubseq(s, words) {
+
+}; 
+
+console.log(numMatchingSubseq("abcde", ["a","bb","acd","ace"]))
+console.log(numMatchingSubseq("dsahjpjauf", ["ahjpjau","ja","ahbwzgqnuk","tnmlanowax"]))
